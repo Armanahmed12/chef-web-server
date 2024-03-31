b93f960f93596f46ed6a4d3a421eafba08601ff8
Armanahmed12 <mdarmanahmed1232@gmail.com>
2024-03-31 22:31:11 +0600

    the details of each chef has been set

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 var cors = require('cors');
 const chefsInfo = require('./data/chefs.json');
+const chefsRecipesData = require('./data/chefsRecipes.json');
 
 
 app.use(cors())
@@ -13,10 +14,19 @@ app.get('/', (req, res) => {
 
 })
 
+
+
 app.get('/chefs', (req, res) => {
  
     res.send(chefsInfo);
 
+})
+
+app.get('/chef/:id', (req, res) =>{
+
+       const id = req.params.id;
+       const chefDetailsInfo =  chefsRecipesData.find(eachChefInfo => eachChefInfo.id == id);
+       res.send(chefDetailsInfo);
 })
 
 app.listen(port, () => {
